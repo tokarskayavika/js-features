cfcdf34aabffd3322ded614309adcbfe8fb3d382
Viktoryia Takarskaya <Viktoryia_Takarskaya@epam.com>
2017-02-22 18:45:13 +0300

    v2

--- index.js
+++ index.js
@@ -1,45 +1,64 @@
-var ListItem = Backbone.Model.extend({
+var ItemModel = Backbone.Model.extend({
     defaults: {
-        title: "My title",
-        price: 0,
-        checked: false
+        name: 'myName',
+        price: 0
+    },
+    validate: function() {
+        if (attrs.price < 0) {
+            console.log('incorrect price');
+        }
     }
 });
 
-var List = Backbone.Collection.extend({
-    model: ListItem
+var ItemCollection = Backbone.Collection.extend({
+    model: ItemModel
 });
 
-var items = new List([
-    new ListItem({
-        title: 'A',
-        price: 100
-    }),
-    new ListItem({
-        title: 'B',
-        price: 200
-    })
-]);
+var itemArray = [{
+    name: 'Scissors',
+    price: 50
+}, {
+    name: 'Knife',
+    price: 100
+}, {
+    name: 'Spoon',
+    price: 150
+}];
+
+var items = new ItemCollection(itemArray);
 
 var ItemView = Backbone.View.extend({
-    tagname: 'li',
-    render: function() {
-        console.log(this);
-        this.$el.html(`<button></button>`);
-    }
-});
-
-var App = Backbone.View.extend({
-    el: $('form'),
+    tagName: 'li',
+    className: 'item',
+    // template: _.template('This is <%= this.name %>'),
     initialize: function() {
-        items.each(function(it) {
-            var view = new ItemView({
-                model: it
-            });
-            console.log(view);
-            $('ul').append(view.render());
-        }, this);
+        this.render();
+        console.log(this);
+    },
+    render: function() {
+        // console.log(this.$el.html('<button>Hello</button>'));
+        // var template = this.template;
+        // console.log(template);
+        // console.log(template(items.at(0).get('name')));
+        // console.log(this.$el);
+        this.$el.html('<button>Hello</button>');
+        return this;
     }
 });
 
-new App();
+var AppView = Backbone.View.extend({
+    // el: $('form'),
+    initialize: function() {
+        new ItemView({});
+        items.each(function(model, index) {
+            console.log(model, index);
+            var itemView = new ItemView(model);
+            // console.log(this.model);
+            // console.log('create new item');
+            // new ItemView(model);
+            $('.list').append(itemView.render());
+        });
+    }
+});
+
+new AppView();
